a38fcbbff85a5d116cf7b3bf26fcf1db0a6a0b46
Steve Salta <stevesalta@mac.lan>
2025-07-06 14:18:44 -0700

    Remove note_modules and build artifacts from git history

--- jules-frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GQj51JAGI828oEnLpiV3JyezFEQ5CsH5kXJBpcAYsrk=\"\n}"
--- jules-frontend/.next/static/chunks/src_app_chat_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_617686ce._.js",
-  "static/chunks/src_f4eaefb8._.js"
-],
-    source: "dynamic"
-});
