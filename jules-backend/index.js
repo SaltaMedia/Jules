64c90aed22e57bf0f324a3762845ac1db8ee5774
Steve Salta <stevesalta@mac.lan>
2025-07-15 13:46:42 -0700

    Add MongoDB connection debugging

--- jules-backend/index.js
+++ jules-backend/index.js
@@ -89,19 +89,23 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 4000;
 
+console.log('About to connect to MongoDB...');
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 // Connect to MongoDB Atlas
+console.log('Calling mongoose.connect...');
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
   console.log('Connected to MongoDB Atlas');
+  console.log('About to start server...');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 })
 .catch((err) => {
   console.error('Failed to connect to MongoDB Atlas:', err);
+  process.exit(1);
 });
